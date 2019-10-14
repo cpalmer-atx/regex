7f54418f22d658f31c2caf2702be1ed34c12deb5
Chad Palmer <chad.n.palmer@gmail.com>
2019-10-14 17:21:40 -0500

    working code

--- mini.php
+++ mini.php
@@ -12,6 +12,17 @@
     $emailRegex = '/[[:space:]]([a-zA-Z]*)@([a-zA-Z]*)\.(com|edu|net)/';
     $webRegex = '/(https:\/\/cs.txstate.edu\/accounts\/profiles\/[a-z0-9]*)/';
 
+    $name = getMatches($html, $nameRegex);
+    $education = getMatches($html, $eduRegex);
+    $research = getMatches($html, $researchRegex);
+    $email = getMatches($html, $emailRegex);
+    $webpage = getMatches($html, $webRegex);
+
+    echo "Name:", $name[0], PHP_EOL;
+    echo "Education: ", $education[2], PHP_EOL;
+    echo "Research interests: ", $research[0], PHP_EOL;
+    echo "Email:", $email[0], PHP_EOL;
+    echo "Webpage: ", $webpage[0], PHP_EOL;
 
     function getMatches ($string, $regex) {
         preg_match_all($regex, $string, $array);
